c519deb9df4cadc87b4b89b03c04aff0c6781579
Szabó Levente <leventeszabo@mail.com>
2013-06-15 21:06:09 +0200

    .gitignore new place; deck implemented; log partially implemented

--- GameModule/src/Log.cpp
+++ GameModule/src/Log.cpp
@@ -3,10 +3,12 @@
 
 void Log::getEventsIterators(list<Event*>::iterator& begin, list<Event*>::iterator& end) const
 {
-	
+	//TODO: ezt mi a rákért nem fogadja el?
+	//begin = events.begin();
+	//end = events.end();
 }
 
 void Log::log(Event* e)
 {
-
+	events.push_front(e);
 }
