4d793efb76aae59e756cbe7f3d4ae3913bf670e7
Csiki <chickenhun@gmail.com>
2013-10-21 23:15:51 +0200

    deploy dir structure designed :D

--- GameModule/src/GameModule.cpp
+++ GameModule/src/GameModule.cpp
@@ -4,14 +4,22 @@
 #include "BotData.h"
 #include "BotDataXMLHandler.h"
 
+/** Belepo pont he!
+  @param argv elemei sorban: logPath, rulesPath, resultsPath, numOfBots, bot1XML, bot2XML...
+*/
 int main(int argc, char* argv[])
 {
+	const char* logPath = argv[1];
+	const char* rulesPath = argv[2];
+	const char* resultsPath = argv[3];
+	int numOfBots = atoi(argv[4]);
+	int* botsID;
 
-	int* playersID = new int[3];
-	playersID[0] = 1;
-	playersID[1] = 2;
-	playersID[2] = 3;
-	GameOwner* go = new GameOwner(3, playersID, "testlog.xml", "testrules.xml", "testresults.xml");
+	int* botsID = new int[3];
+	botsID[0] = 1;
+	botsID[1] = 2;
+	botsID[2] = 3;
+	GameOwner* go = new GameOwner(3, botsID, "testlog.xml", "testrules.xml", "testresults.xml");
 
 	go->initialiseGame();
 
