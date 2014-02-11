45b16ec2e6c3aa959911ed85fe113229509ed13c
csiki <chickenhun@gmail.com>
2014-02-11 22:14:49 +0100

    test_bots finished

--- deploy/www/php/test_bots.php
+++ deploy/www/php/test_bots.php
@@ -5,79 +5,81 @@ include_once("functions.php");
 $botsUnchecked = SQL("SELECT * FROM bots WHERE state = 'processing'");
 if ($botsUnchecked != null)
     foreach ($botsUnchecked as $bot) {
-        /*int botid = atoi(argv[2]);
-        int testcase = atoi(argv[3]);
-        int gameid = atoi(argv[4]);
-        const char* name = argv[5];
-        const char* src = argv[6];
-        BotLanguage lang = static_cast<BotLanguage> (atoi(argv[7]));
-        int numOfKnowledgeTables = atoi(argv[8]);
-        int* knowledgeTables = new int[numOfKnowledgeTables];
-        for (size_t i = 0; i < numOfKnowledgeTables; ++i)
-        {
-            knowledgeTables[i] = atoi(argv[9 + i]);
-        }*/
 
         $accountid = $bot['accountID'];
         $botid = $bot['id'];
+		
+		// compile
+		$src = "../../data/bots/" . $accountid . "/" . $botid;
+		exec("../../exec/compileSO.sh ".$src.".cpp ".$src, $output, $return_val); // compileSO indítása
+		$output = string_replace("'", '"', $output);
+		
+		if ($return_val == 0) // non-zero returnnél van para
+		{
+			// get gameid
+			SQL("INSERT INTO games(checked, leaderboard, rules, log, result, startTime, endTime)
+			VALUES(0, 'test', 'testrules.xml', '', '', 0, 0)");
+			$gameid = $mysqli->insert_id;
+			$resultname = $logname = $gameid . '.xml';
+			SQL("UPDATE games SET log='$logname', result='$resultname'
+			WHERE id=" . $gameid);
 
-        // get gameid
-        SQL("INSERT INTO games(checked, leaderboard, rules, log, result, startTime, endTime)
-		VALUES(0, 'test', 'testrules.xml', '', '', 0, 0)");
-        $gameid = $mysqli->insert_id;
-        $resultname = $logname = $gameid . '.xml';
-        SQL("UPDATE games SET log='$logname', result='$resultname'
-		WHERE id=" . $gameid . "");
+			$name = $bot['name'];
+			$lang = $bot['code_lang'];
+			$numofktables = 0; // for simplicity
+			$ktables = array();
+			$testcase = 0;
 
-        $name = $bot['name'];
-        $src = "../../data/bots/" . $botid . "/" . $botid;
-        $lang = $bot['code_lang'];
-        $numofktables = 0; // for simplicity
-        $ktables = array();
-        $testcase = 0;
+			$output = array();
+			$return_val = 0;
+			$args = $accountid . ' ' . $botid . ' ' . $testcase . ' ' . $gameid . ' ' .
+				$name . ' ' . $src . ' ' . $lang . ' ' . $numofktables . ' ' . explode(' ', $ktables);
+			exec("../../exec/bottester $args", $output, $return_val);
+			echo $return_val;
+			if ($return_val == 0) {
+				// run gamemodule
+				$command = "../../exec/gamemodule " . $gameid;
+				$output = "";
+				$return_val = 0;
 
-        $output = array();
-        $return_val = 0;
-        $args = $accountid . ' ' . $botid . ' ' . $testcase . ' ' . $gameid . ' ' .
-            $name . ' ' . $src . ' ' . $lang . ' ' . $numofktables . ' ' . implode(' ', $ktables);
-        exec("../../exec/bottester $args", $output, $return_val);
-        echo $return_val;
-        if ($return_val == 0) {
-            // run gamemodule
-            $command = "../../exec/gamemodule " . $gameid;
-            $output = "";
-            $return_val = 0;
+				SQL("UPDATE games SET startTime='" . time() . "'
+				WHERE id=" . $this->gameid);
+				exec($command, $output, $return_val);
+				SQL("UPDATE games SET endTime='" . time() . "'
+				WHERE id=" . $this->gameid);
 
-            SQL("UPDATE games SET startTime='" . time() . "'
-			WHERE id=" . $this->gameid);
-            exec($command, $output, $return_val);
-            SQL("UPDATE games SET endTime='" . time() . "'
-			WHERE id=" . $this->gameid);
+				if ($return_val == 4) // everything went alright
+				{
+					// still need to iterate through log,
+					// to check if there's any warning or error on the tested bot
+					$alrighty = true;
+					if (file_exists('../../data/logs/' . $gameid . '.xml')) {
+						$xml = simplexml_load_file('../../data/logs/' . $gameid . '.xml');
 
-            if ($return_val == 4) // everything went alright
-            {
-                // still need to iterate through log,
-                // to check if there's any warning or error on the tested bot
-                $alrighty = true;
-                if (file_exists('../../data/logs/' . $gameid . '.xml')) {
-                    $xml = simplexml_load_file('../../data/logs/' . $gameid . '.xml');
+						foreach ($xml->log->event as $event) {
+							if ((int)$event->logger == $botid && $event->severity == 1) {
+								// error found
+								$alrighty = false;
+								break;
+							}
+						}
 
-                    foreach ($xml->log->event as $event) {
-                        if ((int)$event->logger == $botid && $event->severity == 1) {
-                            // error found
-                            $alrighty = false;
-                            break;
-                        }
-                    }
-
-                    if ($alrighty) {
-                        SQL("UPDATE bots SET state='ok' WHERE id=" . $botid);
-                    } else {
-                        SQL("UPDATE bots SET state='runtime' WHERE id=" . $botid);
-                    }
-                }
-            } else {
-                SQL("UPDATE bots SET state='compilation' WHERE id=" . $botid);
-            }
-        }
-    }
+						if ($alrighty) {
+							SQL("UPDATE bots SET state='ok' WHERE id=" . $botid);
+							SQL("UPDATE bots SET runerror='0' WHERE id=" . $botid);
+						} else {
+							SQL("UPDATE bots SET state='runtime' WHERE id=" . $botid);
+							SQL("UPDATE bots SET runerror='" . $gameid . "' WHERE id=" . $botid);
+						}
+					}
+				} else {
+					SQL("UPDATE bots SET state='runtime' WHERE id=" . $botid);
+					SQL("UPDATE bots SET runerror='" . $gameid . "' WHERE id=" . $botid);
+				}
+			}
+		}
+		else {
+			SQL("UPDATE bots SET state='compilation' WHERE id=" . $botid);
+			SQL("UPDATE bots SET comperror='" . $output . "' WHERE id=" . $botid);
+		}
+	}
