73e83181547d898cf680162326b438c2af58d363
csiki <chickenhun@gmail.com>
2014-02-21 23:41:10 +0100

    bottester fix, not compiled

--- BotTester/BotTester/main.cpp
+++ BotTester/BotTester/main.cpp
@@ -22,10 +22,14 @@ int main(int argc, char* argv[])
     const char* src = argv[6];
     BotLanguage lang = static_cast<BotLanguage> (atoi(argv[7]));
     int numOfKnowledgeTables = atoi(argv[8]);
-    int* knowledgeTables = new int[numOfKnowledgeTables];
-    for (size_t i = 0; i < numOfKnowledgeTables; ++i)
+
+    if (numOfKnowledgeTables > 0)
     {
-        knowledgeTables[i] = atoi(argv[9 + i]);
+        int* knowledgeTables = new int[numOfKnowledgeTables];
+        for (size_t i = 0; i < numOfKnowledgeTables; ++i)
+        {
+            knowledgeTables[i] = atoi(argv[9 + i]);
+        }
     }
 
     BotData botdata(botid, accountid, std::string(name), std::string(src), 1000, lang,
@@ -34,7 +38,10 @@ int main(int argc, char* argv[])
     Tester tester;
     tester.test(&botdata, testcase, gameid);
 
-    delete [] knowledgeTables;
+    if (numOfKnowledgeTables > 0)
+    {
+        delete [] knowledgeTables;
+    }
 
     return 0;
 }
