c286cf5d46c7193f54d6dee1b65a092bada38566
csiki <chickenhun@gmail.com>
2014-02-25 21:31:41 +0100

    dawawd

--- GameModule/croupier_linux/GameDataXMLHandler.cpp
+++ GameModule/croupier_linux/GameDataXMLHandler.cpp
@@ -8,6 +8,7 @@ GameData* GameDataXMLHandler::loadXML(std::string xmlPath)
 	// load file, check if file exists
 	pugi::xml_document doc;
 	pugi::xml_parse_result result = doc.load_file(xmlPath.c_str());
+	std::cerr << xmlPath;
 
 	GameData* gameData = nullptr;
 	if (result)
