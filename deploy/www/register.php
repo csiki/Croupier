dc87e9148f99f1f87a0e77f1a58ec1633ed8663e
csiki <chickenhun@gmail.com>
2014-03-10 19:14:39 +0100

    paff

--- deploy/www/register.php
+++ deploy/www/register.php
@@ -39,7 +39,7 @@ if (isset($_POST['name']) || isset($_POST['email']) || isset($_POST['p']) || iss
 
     if (!$cap->is_valid)
         $errors[] = $tr["ERR_CAPTCHA"];
-    //$errors[] = "A regisztráció még nem nyílt meg!";
+    $errors[] = "A regisztráció még nem nyílt meg!";
     if (count($errors) == 0) {
         $random_salt = hash('sha512', uniqid(mt_rand(1, mt_getrandmax()), true));
         // Create salted password (Careful not to over season)
