769b18f9c609c7bf6bba8b863b0aba6fb95eb911
csiki <chickenhun@gmail.com>
2014-03-26 13:05:16 +0100

    card pics added to show_game, not tested

--- deploy/www/php/head.php
+++ deploy/www/php/head.php
@@ -5,3 +5,4 @@
     <script type="text/javascript" src="/scripts/jquery-latest.min.js"></script>
     <script type="text/javascript" src="/scripts/jquery-ui.min.js"></script>
     <link rel="stylesheet" type="text/css" href="/style/main.css">
+    <link rel="icon" type="image/png" href="https://poker.sch.bme.hu/images/favicon.ico">
